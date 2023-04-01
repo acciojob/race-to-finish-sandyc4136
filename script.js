de47e540a12c7d8f6497664f655bf88a14ebf6e6
Sandip Chhetry <sandyc4136@gmail.com>
2023-04-01 13:29:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-window.promises = [promise1, promise2, promise3, promise4, promise5];
+window.promises = [];
 
- const promise1= new Promise((resolve) => setTimeout(() => resolve('Promise 1 resolved'), Math.random() * 5000 + 1000));
-  const promise2= new Promise((resolve) => setTimeout(() => resolve('Promise 2 resolved'), Math.random() * 5000 + 1000));
-  const promise3= new Promise((resolve) => setTimeout(() => resolve('Promise 3 resolved'), Math.random() * 5000 + 1000));
-  const promise4= new Promise((resolve) => setTimeout(() => resolve('Promise 4 resolved'), Math.random() * 5000 + 1000));
-  const promise5= new Promise((resolve) => setTimeout(() => resolve('Promise 5 resolved'), Math.random() * 5000 + 1000));
+  new Promise((resolve) => setTimeout(() => resolve('Promise 1 resolved'), Math.random() * 5000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 2 resolved'), Math.random() * 5000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 3 resolved'), Math.random() * 5000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 4 resolved'), Math.random() * 5000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 5 resolved'), Math.random() * 5000 + 1000)),
 
 
 Promise.any(promises).then((result) => {
