230b5e67fcb07ef196c1d691eb5fa2f1cf17c217
Sandip Chhetry <sandyc4136@gmail.com>
2023-04-01 13:33:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 window.promises = [];
 
-  new Promise((resolve) => setTimeout(() => resolve('Promise 1 resolved'), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve('Promise 2 resolved'), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve('Promise 3 resolved'), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve('Promise 4 resolved'), Math.random() * 5000 + 1000)),
-  new Promise((resolve) => setTimeout(() => resolve('Promise 5 resolved'), Math.random() * 5000 + 1000)),
+let random = Math.floor(Math.random()*6)
 
+let promise1 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("1");
+  },random * 1000)
+})
 
-Promise.any(promises).then((result) => {
-  document.getElementById('output').innerHTML = result;
-}).catch((error) => {
-  console.error(error);
-});
+let promise2 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("2");
+  },random * 1000)
+})
+
+let promise3 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("3");
+  },random * 1000)
+})
+
+let promise4 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("4");
+  },random * 1000)
+})
+
+let promise5 = new Promise((resolve, reject)=>{
+  setTimeout(()=>{
+        resolve("5");
+  },random * 1000)
+})
+
+promises =[promise1,promise2,promise3,promise4,promise5]
+Promise.any(promises).then(data=>{
+  document.getElementById("output").innerHTML = data;
+})
 // Do not change the code above this
 // add your promises to the array `promises`
